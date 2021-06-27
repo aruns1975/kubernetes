165640c0e90e6ba9412693f52a0b6becfff98f19
Arun <aruns1975@gmail.com>
2021-06-27 23:49:01 +0530

    Added load balancing for Kubernetes dashboard

--- Local_cluster/scripts/loadBalancer.sh
+++ Local_cluster/scripts/loadBalancer.sh
@@ -12,28 +12,31 @@ frontend kubernetes-frontend
     mode tcp
     option tcplog
     default_backend kubernetes-backend
+
+frontend kubernetes-dashboard-frontend
+    bind 10.1.0.100:8443
+    mode tcp
+    option tcplog
+    default_backend kubernetes-dashboard-backend
+
+EOF
+
+echo "[edit /etc/haproxy/haproxy.cfg] -> Adding kubernetes-backend"
+cat <<EOF | sudo tee -a /etc/haproxy/haproxy.cfg
+
 backend kubernetes-backend
     mode tcp
     option tcp-check
     balance roundrobin
     server master-node-1 10.1.0.101:6443 check fall 3 rise 2
     server master-node-2 10.1.0.102:6443 check fall 3 rise 2
-EOF
-
-echo "[edit /etc/haproxy/haproxy.cfg] -> Adding kubernetes-backend"
-cat <<EOF | sudo tee -a /etc/haproxy/haproxy.cfg
-dashboard-frontend kubernetes-dashboard-frontend
-    bind 10.1.0.100:8443
-    mode tcp
-    option tcplog
-    default_backend kubernetes-dashboard-backend
-dashboard-backend kubernetes-dhasboard-backend
+	
+backend kubernetes-dashboard-backend
     mode tcp
     option tcp-check
     balance roundrobin
     server master-node-1 10.1.0.101:8443 check fall 3 rise 2
     server master-node-2 10.1.0.102:8443 check fall 3 rise 2
-
 EOF
 
-systemctl restart haproxy
+sudo systemctl start haproxy
