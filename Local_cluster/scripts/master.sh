9159871ae75a0d20de9c4b93ca88df2d5f5efa0a
Arun <aruns1975@gmail.com>
2021-06-28 21:12:21 +0530

    Added metellb so that the loadbalance service can be created on the bare metal on premises kubernates cluster.
    The Load balancer has been configured with two pools
    default - 10.0.0.50-10.0.0.90
    production-pool - 10.0.0.110-10.0.0.150
    For eg.
    apiVersion: v1
    kind: Service
    metadata:
      name: nginx
      annotations:
        metallb.universe.tf/address-pool: production-pool
    spec:
      ports:
      - port: 80
        targetPort: 80
      selector:
        app: nginx
      type: LoadBalancer

--- Local_cluster/scripts/master.sh
+++ Local_cluster/scripts/master.sh
@@ -140,3 +140,29 @@ spec:
   selector:
     app: nginx
 EOF
+
+echo '[METALLB] Creating the name space for the metallb'
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.10.2/manifests/namespace.yaml
+
+echo '[METALLB] Installating metallb'
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.10.2/manifests/metallb.yaml
+
+echo '[METALLB] Configuring metallb pools'
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  namespace: metallb-system
+  name: config
+data:
+  config: |
+    address-pools:
+    - name: default
+      protocol: layer2
+      addresses:
+      - 10.0.0.50-10.0.0.90
+    - name: production-pool
+      protocol: layer2
+      addresses:
+      - 10.0.0.110-10.0.0.150
+EOF
